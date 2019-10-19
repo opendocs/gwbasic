a5dc7eeddc13e0b0885cef330d646629a823890e
Susam Pal <susam@susam.in>
2019-10-20 01:30:57 +0530

    Fix

--- fix-charset.py
+++ fix-charset.py
@@ -4,6 +4,7 @@ import glob
 def fix_charset(path):
     with open(path, 'rb') as f:
         html = f.readlines()
+    html[0] = (b'<!DOCTYPE html>\r\n<html>\r\n')
     html[4] = (b'<meta http-equiv="Content-Type" content="text/html; '
                b'charset=ISO-8859-1">\r\n</head>\r\n')
     html = b''.join(html)
